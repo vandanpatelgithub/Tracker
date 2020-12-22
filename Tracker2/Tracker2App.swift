0a88f894c6d1a0ae849e60a12404252844726484
Vandan Patel <vandan.ets@gmail.com>
2020-12-22 18:18:32 -0500

    Imports assets and implements details screen.

--- Tracker2/Tracker2App.swift
+++ Tracker2/Tracker2App.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Tracker2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
