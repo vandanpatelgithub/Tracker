bdefe84c2bc06c685303b94cd4c277fee812935b
Vandan Patel <vandan.ets@gmail.com>
2020-12-22 18:12:32 -0500

    Initial Commit

---
+++ Tracker2/Tracker2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker2App.swift
+//  Tracker2
+//
+//  Created by Vandan Patel on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Tracker2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
